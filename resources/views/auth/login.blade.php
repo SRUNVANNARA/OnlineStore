a9abb300233842aa93a305c17a8f7dcb43e06574
sub <sub@gmail.com>
2020-04-14 19:19:21 +0700

    Alsmost done for login and register features.

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html class="loading" lang="en" data-textdirection="ltr">
+<!-- BEGIN: Head-->
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
+    <meta name="description" content="Vuexy admin is super flexible, powerful, clean &amp; modern responsive bootstrap 4 admin template with unlimited possibilities.">
+    <meta name="keywords" content="admin template, Vuexy admin template, dashboard template, flat admin template, responsive admin template, web app">
+    <meta name="author" content="PIXINVENT">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Dashboard analytics - Vuexy - Bootstrap HTML admin template</title>
+    <link rel="apple-touch-icon" href="{{ asset('app-asset/images/ico/apple-icon-120.png') }}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('app-asset/images/ico/favicon.ico') }}">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600" rel="stylesheet">
+
+    <!-- BEGIN: Vendor CSS-->
+    <link rel="stylesheet" type="text/css" href="{{asset('app-asset/vendors/css/vendors.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('app-asset/vendors/css/charts/apexcharts.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('app-asset/vendors/css/extensions/tether-theme-arrows.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('app-asset/vendors/css/extensions/tether.min.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('app-asset/vendors/css/extensions/shepherd-theme-default.css')}}">
+    <!-- END: Vendor CSS-->
+
+    <!-- BEGIN: Theme CSS-->
+    <link rel="stylesheet" type="text/css" href="">
+    <link rel="stylesheet" type="text/css" href="{{asset('app-asset/css/bootstrap.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('app-asset/css/bootstrap-extended.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('app-asset/css/colors.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('app-asset/css/components.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('app-asset/css/themes/dark-layout.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('app-asset/css/themes/semi-dark-layout.css')}}">
+
+    <!-- BEGIN: Page CSS-->
+    <link rel="stylesheet" type="text/css" href="{{asset('app-asset/css/core/menu/menu-types/vertical-menu.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('app-asset/css/core/colors/palette-gradient.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('app-asset/css/pages/dashboard-analytics.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('app-asset/css/pages/card-analytics.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('app-asset/css/plugins/tour/tour.css')}}">
+    <!-- END: Page CSS-->
+
+</head>
+<!-- END: Head-->
+
+<!-- BEGIN: Body-->
+
+<body class="vertical-layout vertical-menu-modern dark-layout 1-column  navbar-floating footer-static bg-full-screen-image  blank-page blank-page" data-open="click" data-menu="vertical-menu-modern" data-col="1-column" data-layout="dark-layout">
+    <!-- BEGIN: Content-->
+    <div class="app-content content">
+        <div class="content-overlay"></div>
+        <div class="header-navbar-shadow"></div>
+        <div class="content-wrapper">
+            <div class="content-header row">
+            </div>
+            <div class="content-body">
+                <section class="row flexbox-container">
+                    <div class="col-xl-8 col-11 d-flex justify-content-center">
+                        <div class="card bg-authentication rounded-0 mb-0">
+                            <div class="row m-0">
+                                <div class="col-lg-6 d-lg-block d-none text-center align-self-center px-1 py-0">
+                                    <img src="{{asset('app-asset/images/pages/login.png')}}" alt="branding logo">
+                                </div>
+                                <div class="col-lg-6 col-12 p-0">
+                                    <div class="card rounded-0 mb-0 px-2">
+                                        <div class="card-header pb-1">
+                                            <div class="card-title">
+                                                <h4 class="mb-0">Login</h4>
+                                            </div>
+                                        </div>
+                                        <p class="px-2">Welcome back, please login to your account.</p>
+                                        <div class="card-content">
+                                            <div class="card-body pt-1">
+                                                <form method="POST" action="{{ route('login') }}">
+                                                    @csrf
+                                                    <fieldset class="form-label-group form-group position-relative has-icon-left">
+                                                        <input type="text" class="form-control" id="user-name" name="email" placeholder="Username" required  autocomplete="email" autofocus>
+                                                        <div class="form-control-position">
+                                                            <i class="feather icon-user"></i>
+                                                        </div>
+                                                        <label for="user-name">Username</label>
+                                                    </fieldset>
+
+                                                    <fieldset class="form-label-group position-relative has-icon-left">
+                                                        <input type="password" class="form-control" id="user-password" name="password" placeholder="Password"  required autocomplete="current-password">
+                                                        <div class="form-control-position">
+                                                            <i class="feather icon-lock"></i>
+                                                        </div>
+                                                        <label for="user-password">Password</label>
+                                                    </fieldset>
+                                                    <div class="form-group d-flex justify-content-between align-items-center">
+                                                        <div class="text-left">
+                                                            <fieldset class="checkbox">
+                                                                <div class="vs-checkbox-con vs-checkbox-primary">
+                                                                    <input type="checkbox">
+                                                                    <span class="vs-checkbox">
+                                                                        <span class="vs-checkbox--check">
+                                                                            <i class="vs-icon feather icon-check"></i>
+                                                                        </span>
+                                                                    </span>
+                                                                    <span class="">Remember me</span>
+                                                                </div>
+                                                            </fieldset>
+                                                        </div>
+                                                        <div class="text-right"><a href="auth-forgot-password.html" class="card-link">Forgot Password?</a></div>
+                                                    </div>
+                                                    <a href="/register" class="btn btn-outline-primary float-left btn-inline">Register</a>
+                                                    <button type="submit" class="btn btn-primary float-right btn-inline">Login</button>
+                                                </form>
+                                            </div>
+                                        </div>
+                                        <div class="login-footer">
+                                            <div class="divider">
+                                                {{--<div class="divider-text">OR</div>--}}
+                                            </div>
+                                            {{--<div class="footer-btn d-inline">--}}
+                                                {{--<a href="#" class="btn btn-facebook"><span class="fa fa-facebook"></span></a>--}}
+                                                {{--<a href="#" class="btn btn-twitter white"><span class="fa fa-twitter"></span></a>--}}
+                                                {{--<a href="#" class="btn btn-google"><span class="fa fa-google"></span></a>--}}
+                                                {{--<a href="#" class="btn btn-github"><span class="fa fa-github-alt"></span></a>--}}
+                                            {{--</div>--}}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+            </div>
+        </div>
+    </div>
+    <!-- END: Content-->
+
+
+    <!-- BEGIN: Vendor JS-->
+    <script src="{{asset('app-asset/vendors/js/vendors.min.js')}}"></script>
+    <!-- BEGIN Vendor JS-->
+
+    <!-- BEGIN: Page Vendor JS-->
+    {{--<script src="{{asset('app-asset/vendors/js/charts/apexcharts.min.js')}}"></script>--}}
+    <script src="{{asset('app-asset/vendors/js/extensions/tether.min.js')}}"></script>
+    <script src="{{asset('app-asset/vendors/js/extensions/shepherd.min.js')}}"></script>
+    <!-- END: Page Vendor JS-->
+
+    <!-- BEGIN: Theme JS-->
+    <script src="{{asset('app-asset/js/core/app-menu.js')}}"></script>
+    <script src="{{asset('app-asset/js/core/app.js')}}"></script>
+    <script src="{{asset('app-asset/js/scripts/components.js')}}"></script>
+    <!-- END: Theme JS-->
+
+    <!-- BEGIN: Page JS-->
+    {{--<script src="{{asset('app-asset/js/scripts/pages/dashboard-analytics.js')}}"></script>--}}
+    <!-- END: Page JS-->
+
+</body>
+<!-- END: Body-->
+
+</html>
